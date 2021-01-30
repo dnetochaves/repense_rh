9ea1f34eae086e1db7b575cd56e42e72d08d82fc
dnetochaves <dnetochaves@gmail.com>
2021-01-30 15:46:44 -0300

    Refactor

--- apps/employee/models.py
+++ apps/employee/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from apps.departament.models import Departament
 from apps.company.models import Company
 from django.urls import reverse
+from django.db.models import Avg, Count, Min, Sum
+
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
@@ -10,10 +12,16 @@ class Employee(models.Model):
     departament = models.ManyToManyField(Departament)
     company = models.ForeignKey(Company, on_delete=models.PROTECT)
     cpf = models.CharField(max_length=50, null=True, blank=True)
+    photo = models.ImageField(upload_to='photo')
+
+    @property
+    def sum_overtime(self):
+        total = self.ouvertimerecord_set.all().aggregate(Sum('hours'))[
+            'hours__sum']
+        return total
 
     def get_absolute_url(self):
         return reverse('employee:list_employee')
 
     def __str__(self):
         return self.name
-     
